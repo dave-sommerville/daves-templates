b15172c40fd4218c0406c578f6ebad433599bf6f
Dave Sommerville <dave.r.sommerville@outlook.com>
2024-12-06 08:03:07 -0600

    Score Class added for future reference

---
+++ js/score-class.js
@@ -0,0 +1,34 @@
+'use strict';
+
+class Score {
+	#date;
+	#hits;
+	#percentage;
+
+	constructor(date, hits, percentage){
+		this.date = date;
+		this.hits = hits;
+		this.percentage = percentage;
+	}
+
+	set date(date){
+		this.#date = date;
+	}
+	set hits(hits) {
+		this.#hits = hits;
+	}
+	set percentage(percentage) {
+		this.#percentage = percentage;
+	}
+
+	get date() {
+		return this.#date;
+	}
+	get hits() {
+		return this.#hits;
+	}
+	get percentage() {
+		return this.#percentage;
+	}
+}
+
